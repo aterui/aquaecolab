dbcd39f40feb8de22450e0fdf273d38400105ec7
AkiraTerui <hanabi0111@gmail.com>
2020-07-04 00:16:00 -0400

    Edits: index

--- logo.R
+++ logo.R
@@ -2,10 +2,10 @@
 x <- rnorm(500)
 y <- rnorm(500)
 
-png(filename = "image/logo.png", width = 500, height = 500)
+png(filename = "image/logo.png", width = 100, height = 100)
 par(mar = c(0,0,0,0))
 plot(y ~ x, ann = F, axes = F,
      ylim = c(-1, 1), xlim = c(-1, 1),
      pch = 21, bg = heat.colors(500, alpha = 0.5),
-     cex = rnorm(500, sd = 15), col = NA)
+     cex = rnorm(500, sd = 5), col = NA)
 dev.off()
