a1a14feec4477f2a08dfca751a78fd0d20e49d97
AkiraTerui <hanabi0111@gmail.com>
2020-07-29 19:06:17 -0400

    Create report01_temp.R

---
+++ file/report01_temp.R
@@ -0,0 +1,60 @@
+#' ---
+#' title: "Report Title"
+#' author: Your Name
+#' date: Date
+#' output: html_document
+#' ---
+
+#' ### 2.5.1 Vector
+#' a. Create three numeric vectors with length 3, 6 and 20, respectively. Each vector must be created using different functions in R.
+
+  #ENTER YOUR ANSWER HERE (delete this message when you submit)
+
+#' b. Create three character vectors with length 3, 6 and 20, respectively. Each vector must be created using different functions in R.
+
+  #ENTER YOUR ANSWER HERE (delete this message when you submit)
+
+#' c. Copy the following script to your R script and perform the following analysis:
+#' - Identify element IDs of x that are greater than 2.0 
+#' - Identify element values of x that are greater than 2.0
+
+  #ENTER YOUR ANSWER HERE (delete this message when you submit)
+
+
+#' ### 2.5.2 Matrix
+#' a. Create a numeric matrix with 4 rows and 4 columns. Each column must contain identical elements.
+
+  #ENTER YOUR ANSWER HERE (delete this message when you submit)
+
+#' b. Create a numeric matrix with 4 rows and 4 columns. Each row must contain identical elements.
+
+  #ENTER YOUR ANSWER HERE (delete this message when you submit)
+
+#' c. Create a character matrix with 4 rows and 4 columns. Each column must contain identical elements.
+
+  #ENTER YOUR ANSWER HERE (delete this message when you submit)
+
+#' d. Create a character matrix with 4 rows and 4 columns. Each row must contain identical elements.
+
+  #ENTER YOUR ANSWER HERE (delete this message when you submit)
+
+#' e. Copy the following script to your R script and perform the following analysis:
+#' - Identify element IDs of x that are greater than 2.0 (specify row and column IDs)
+#' - Identify element values of x that are greater than 2.0 and caluculate the mean.
+
+  #ENTER YOUR ANSWER HERE (delete this message when you submit)
+
+
+#' ### 2.5.3 Dataframe
+#' a. Create a dataframe with 3 variables (name variables as x, y and z. x must be character while y and z must be numeric.
+
+  #ENTER YOUR ANSWER HERE (delete this message when you submit)
+
+#' b. Check the data structure (higher-level) of x, y and z
+
+  #ENTER YOUR ANSWER HERE (delete this message when you submit)
+
+#' c. Copy the following script to your R script and perform the following analysis:
+#' - Calculate means of temperature and abundance for states VA and NC separately - use tapply() function
+
+  #ENTER YOUR ANSWER HERE (delete this message when you submit)
